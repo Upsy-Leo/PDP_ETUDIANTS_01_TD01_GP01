a7ac88354db365b63f307310dd70b525f5db59ec
Leo_Upsy <leopold.andre@edu.ece.fr>
2026-01-07 10:26:50 +0100

    init etape 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
 
+// Define the pins that we will use
+#define CAPTEUR 33
+#define LED 26
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
-  // put your code here to run repeatedly
+  // Effectuer une mesure toutes les 5s
+
+  // Afficher l'humidité relative avec un peu de formatage
+
+  // Afficher la température avec un peu de formatage
 }
